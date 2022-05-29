b5d9a42c6d45977d585629c5cdea98bf9fd4aa2a
sebasbit <sebastian.torres0426@gmail.com>
2022-05-29 12:49:57 -0500

    Add unit test structure

---
+++ tests/Unit/Service/Bingo/BingoCreateServiceTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Unit\Service\Bingo;
+
+use PHPUnit\Framework\TestCase;
+
+class BingoCreateServiceTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
