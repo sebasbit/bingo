4cbc20d1aa2002838eec1c1e87dfb58601961370
sebasbit <sebastian.torres0426@gmail.com>
2022-05-30 21:55:55 -0500

    Refactor BingoCreateServiceTest

--- tests/Unit/Service/Bingo/BingoCreateServiceTest.php
+++ tests/Unit/Service/Bingo/BingoCreateServiceTest.php
@@ -3,75 +3,53 @@
 namespace App\Tests\Unit\Service\Bingo;
 
 use PHPUnit\Framework\TestCase;
+use App\Entity\BingoId;
 use App\Service\Bingo\BingoCreateInput;
 use App\Service\Bingo\BingoCreateService;
 
 class BingoCreateServiceTest extends TestCase
 {
-    public function test_return_a_bingo_with_the_title_passed_by_parameter()
+    public function test_return_a_bingo_with_an_id()
     {
-        // assign
+        $input = new BingoCreateInput(1, 'bingo-bongo');
         $bingoCreateService = new BingoCreateService();
-        $bingoCreateInput = new BingoCreateInput(1, 'bingo-bongo');
-        // act
-        $bingo = $bingoCreateService->execute($bingoCreateInput);
-        // asert
-        $this->assertSame($bingoCreateInput->title(), $bingo->title());
+
+        $bingo = $bingoCreateService->execute($input);
+
+        $this->assertInstanceOf(BingoId::class, $bingo->id());
     }
 
-    public function test_return_a_bingo_with_the_user_passed_by_parameter()
+    public function test_return_a_bingo_with_the_values_passed_by_parameter()
     {
-        // assign
+        $input = new BingoCreateInput(1, 'bingo-bongo');
         $bingoCreateService = new BingoCreateService();
-        $bingoCreateInput = new BingoCreateInput(1, 'bingo-bongo');
-        // act
-        $bingo = $bingoCreateService->execute($bingoCreateInput);
-        // asert
-        $this->assertSame($bingoCreateInput->userId(), $bingo->userId());
+
+        $bingo = $bingoCreateService->execute($input);
+
+        $this->assertSame($input->userId(), $bingo->userId());
+        $this->assertSame($input->title(), $bingo->title());
     }
 
-    public function test_return_a_bingo_with_default_value_to_font_color()
+    public function test_return_a_bingo_with_default_values()
     {
-        // assign
+        $input = new BingoCreateInput(1, 'bingo-bongo');
         $bingoCreateService = new BingoCreateService();
-        $bingoCreateInput = new BingoCreateInput(1, 'bingo-bongo');
-        // act
-        $bingo = $bingoCreateService->execute($bingoCreateInput);
-        // asert
+
+        $bingo = $bingoCreateService->execute($input);
+
         $this->assertSame('000000', $bingo->fontColor());
-    }
-
-    public function test_return_a_bingo_with_default_value_to_backgound_color()
-    {
-        // assign
-        $bingoCreateService = new BingoCreateService();
-        $bingoCreateInput = new BingoCreateInput(1, 'bingo-bongo');
-        // act
-        $bingo = $bingoCreateService->execute($bingoCreateInput);
-        // asert
         $this->assertSame('FFFFFF', $bingo->backgoundColor());
-    }
-
-    public function test_return_a_bingo_with_default_value_to_picture()
-    {
-        // assign
-        $bingoCreateService = new BingoCreateService();
-        $bingoCreateInput = new BingoCreateInput(1, 'bingo-bongo');
-        // act
-        $bingo = $bingoCreateService->execute($bingoCreateInput);
-        // asert
         $this->assertNull($bingo->picture());
     }
 
     public function test_return_a_bingo_with_24_empty_boxes()
     {
-        // assign
+        $input = new BingoCreateInput(1, 'bingo-bongo');
         $bingoCreateService = new BingoCreateService();
-        $bingoCreateInput = new BingoCreateInput(1, 'bingo-bongo');
-        // act
-        $bingo = $bingoCreateService->execute($bingoCreateInput);
+
+        $bingo = $bingoCreateService->execute($input);
         $boxes = $bingo->boxes();
-        // asert
+
         $this->assertCount(24, $boxes);
 
         for ($i = 0; $i < 24; $i++) {
@@ -80,16 +58,5 @@ class BingoCreateServiceTest extends TestCase
             $this->assertSame($i, $box->order());
             $this->assertSame('', $box->content());
         }
-    }
-
-    public function test_return_a_bingo_with_id()
-    {
-        // assign
-        $bingoCreateService = new BingoCreateService();
-        $bingoCreateInput = new BingoCreateInput(1, 'bingo-bongo');
-        // act
-        $bingo = $bingoCreateService->execute($bingoCreateInput);
-        // asert
-        $this->assertNotNull($bingo->id());
     }
 }
